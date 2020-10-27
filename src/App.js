5552cd1587df5c086d38ba8f2ac8fdfd14792f74
04vibs <04vibs@gmail.com>
2020-10-27 12:29:15 +0530

    Initial-commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end Javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favourite Js library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+];
+
+const options = [
+    {
+        label: 'The color Red',
+        value: 'red'
+    },
+    {
+        label: 'The color Green',
+        value: 'green'
+    },
+    {
+        label: 'A shade of blue',
+        value: 'blue'
+    }
+];
+
+export default () => {
+    return (
+        <div>
+            {/* <Accordion items={items}/> */}
+            {/* <Search/> */}
+            <Dropdown options={options}  
+            />
+        </div>
+    );
+};
