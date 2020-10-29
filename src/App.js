13ef501dd2b5d35df1fa55cc5cfca8cdbb21aade
04vibs <04vibs@gmail.com>
2020-10-29 14:39:30 +0530

    widgets-completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/translate';
+
 
 const items = [
     {
@@ -34,12 +36,28 @@ const options = [
 ];
 
 export default () => {
+
+    // const [selected, setSelected] = useState(options[0]);
+    // const [showDropdown, setShowDropdown] = useState(true);
+
     return (
+        // <div>
+        //     <Accordion items={items}/>
+        //     {/* <Search/> */}
+
+        //     <button onClick={()=>setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+        //     {showDropdown ?
+        //         <Dropdown 
+        //         selected = {selected}
+        //         onSelectedChange = {setSelected}
+        //         options={options}  
+        //     />: null
+        //     }
+        // </div>
+
+
         <div>
-            {/* <Accordion items={items}/> */}
-            {/* <Search/> */}
-            <Dropdown options={options}  
-            />
+            <Translate/>
         </div>
     );
 };
